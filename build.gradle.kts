68e29a2fe7613d8518961b135bac0febafe1aebd
woghd9072 <woghd9072@gmail.com>
2021-03-16 20:45:32 +0900

    Add h2 & jpa

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
 	implementation("com.graphql-java:graphql-spring-boot-starter:5.0.2")
 	implementation("com.graphql-java:graphql-java-tools:5.2.4")
 
+	implementation("com.h2database:h2")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
