ede9c21a58116bcc25f673b85677abeb07ff2834
woghd9072 <woghd9072@gmail.com>
2021-03-16 19:33:14 +0900

    불필요코드 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,6 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
--- src/main/kotlin/com/jaehong/Application.kt
+++ src/main/kotlin/com/jaehong/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class GraphQlApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<GraphQlApplication>(*args)
+	runApplication<Application>(*args)
 }
