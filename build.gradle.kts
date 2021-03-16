8b0bcd4fd90261cf09d198ca4d2b1b38d1c6f08f
woghd9072 <woghd9072@gmail.com>
2021-03-16 20:05:03 +0900

    Add graphql spring boot starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.3"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id ("com.apollographql.apollo") version "2.5.4"
 	kotlin("jvm") version "1.4.30"
 	kotlin("plugin.spring") version "1.4.30"
 }
@@ -20,6 +21,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("com.graphql-java:graphql-spring-boot-starter:5.0.2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
