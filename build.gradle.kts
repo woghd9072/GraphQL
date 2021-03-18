98115b51945a8ede3db1c6cfc53911a2a96324d3
woghd9072 <woghd9072@gmail.com>
2021-03-18 15:32:40 +0900

    build.gradle & yaml 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,7 @@ dependencies {
 
 	implementation("com.graphql-java:graphql-spring-boot-starter:5.0.2")
 	implementation("com.graphql-java:graphql-java-tools:5.2.4")
-
-	implementation("com.h2database:h2")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation ("com.graphql-java:graphql-java-spring-boot-starter-webmvc:1.0")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
