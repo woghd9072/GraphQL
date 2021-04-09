872d8b570c8f1af7a4f498910e0239cb41a7401e
woghd9072 <woghd9072@gmail.com>
2021-04-09 14:19:47 +0900

    Add SQL file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO author (id, first_name, last_name) VALUES (1, 'Anne', 'Rice');
+INSERT INTO author (id, first_name, last_name) VALUES (2, 'Herman', 'Melville');
+INSERT INTO author (id, first_name, last_name) VALUES (3, 'Joanne', 'Rowling');
+
+INSERT INTO book (id, name, page_count, author_id) VALUES (1, 'Harry Potter and the Philosopher''s Stone', 187, 1);
+INSERT INTO book (id, name, page_count, author_id) VALUES (2, 'Interview with the vampire', 685, 2);
+INSERT INTO book (id, name, page_count, author_id) VALUES (3, 'Moby Dick', 330, 3);
